c13f1d05475b05629061f66d30918ff7d026d341
Jack Jain <cirocelot@gmail.com>
2018-11-18 22:25:36 -0700

    added smurfify button

---
+++ files/script.js
@@ -0,0 +1,13 @@
+function highlight(){
+	let navy = document.getElementById("navySeal");
+	let arr = navy.split(' ');
+	arr.map
+
+}
+
+function smurfify() {
+	let navy = document.getElementById("navySeal");
+	navy.textContent = navy.textContent.replace(/fucking/g, "smurfing");
+	navy.textContent = navy.textContent.replace(/bitch/g, "smurf");
+	navy.textContent = navy.textContent.replace(/fuck/g, "smurf")
+}
